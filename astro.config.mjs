c510745241c284cbc15c2b3ca25bd5272b007c49
EveSunMaple <evesunmaple@outlook.com>
2025-01-09 14:29:17 +0800

    feat: upgrade to Astro v5 ……project rewrite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,46 @@
-import { defineConfig } from "astro/config";
-import terser from "@rollup/plugin-terser";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
-
+import terser from "@rollup/plugin-terser";
+import swup from "@swup/astro";
 import astroI18next from "astro-i18next";
 
-import swup from "@swup/astro";
-import dotenv from 'dotenv';
+import icon from "astro-icon";
+
+import { defineConfig } from "astro/config";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://shuoshuo.saroprock.com",
-    style: {
-        scss: {
-            includePaths: ["./src/styles"],
-        },
+  site: "https://shuoshuo.saroprock.com",
+  style: {
+    scss: {
+      includePaths: ["./src/styles"],
     },
-    integrations: [
-        swup({
-            cache: true,
-            progress: true,
-            accessibility: true,
-            smoothScrolling: true,
-            preload: {
-                hover: true,
-                visible: false
-            },
-            theme: 'slide',
-            containers: ["#swup"],
-        }),
-        terser({
-            compress: true,
-            mangle: true,
-        }),
-        sitemap(),
-        tailwind(),
-        astroI18next(),
-        playformCompress(),
-    ],
+  },
+  integrations: [
+    icon(),
+    swup({
+      cache: true,
+      progress: true,
+      accessibility: true,
+      smoothScrolling: true,
+      preload: {
+        hover: true,
+        visible: false,
+      },
+      theme: "slide",
+      containers: ["#swup"],
+    }),
+    terser({
+      compress: true,
+      mangle: true,
+    }),
+    sitemap(),
+    tailwind(),
+    astroI18next(),
+    playformCompress(),
+  ],
 });
